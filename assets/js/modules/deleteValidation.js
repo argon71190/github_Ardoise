d8fc5c2014e59a580d272e47b241c070ab2feb63
Bluebender <olliviev@gmail.com>
2023-04-18 11:14:43 +0200

    Modal du delete customer avec closed quand on clique à l'exterieur du modal et un backdrop pour empêcher de cicker sur les butons en arrière plan

--- assets/js/modules/deleteValidation.js
+++ assets/js/modules/deleteValidation.js
@@ -1,14 +1,20 @@
 
 function deleteValidation(){
-    let deleteCustomer = document.getElementById('deleteCustomer');
-    let deleteValidation = document.getElementById('deleteValidation');
-    let deleteCancel = document.getElementById('deleteCancel');
+    let deleteBtn = document.getElementById('deleteBtn');
+    let modal = document.getElementById('modal');
+    let cancelBtn = document.getElementById('cancelBtn');
+    let cancelIcon = document.getElementById('cancelIcon');
+    let backdrop = document.getElementById('backdrop');
 
-    deleteCustomer.addEventListener('click', function(){
-        deleteValidation.style.display='block';
-    });
-    deleteCancel.addEventListener('click', function(){
-        deleteValidation.style.display='none';
+    window.addEventListener('click', function(e){
+        if(deleteBtn.contains(e.target)){
+            modal.style.display='block';
+            backdrop.style.display='block';
+        }
+        else if(!modal.contains(e.target) || cancelBtn.contains(e.target) || cancelIcon.contains(e.target)){
+            modal.style.display='none';
+            backdrop.style.display='none';
+        }
     });
 }
 
