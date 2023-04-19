84ed247678e194caad38cdaa449236ca9480e72c
Brecosis <raph29490@gmail.com>
2023-04-19 12:03:09 +0200

    Essai stat

---
+++ assets/js/display.js
@@ -0,0 +1,20 @@
+ function toggleDisplay(){
+
+    let divOptions = document.closest('.divOptions');
+
+            console.log('hello');
+            if(divOptions.style.display == "none"){
+                divOptions.style.display = "block";
+            }
+            else{
+                divOptions.style.display = "none";
+            }
+}
+
+
+    
+
+
+
+
+
